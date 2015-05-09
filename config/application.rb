d599c4eecd53edba05c3d1c9040c479b6871759c
duncangarde <duncangarde@hotmail.com>
2015-05-09 18:48:09 +0100

    taking out jsonp middleware

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-require 'rack/jsonp'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -22,6 +22,6 @@ module Netapet
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.middleware.use Rack::JSONP
+    
   end
 end
